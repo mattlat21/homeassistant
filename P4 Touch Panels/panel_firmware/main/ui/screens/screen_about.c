47f6ac7b08629ef583559ebf698f26838b52103e
Matt <mattlat21@gmail.com>
2026-05-11 15:13:59 +1000

    Update about screen

--- P4 Touch Panels/panel_firmware/main/ui/screens/screen_about.c
+++ P4 Touch Panels/panel_firmware/main/ui/screens/screen_about.c
@@ -24,7 +24,7 @@ static void about_refresh_labels(void)
 {
     if (s_lbl_version != NULL) {
         char buf[48];
-        snprintf(buf, sizeof(buf), "Firmware: %u.%u.%u", (unsigned)FW_VER_MAJOR, (unsigned)FW_VER_MINOR,
+        snprintf(buf, sizeof(buf), "Firmware v%u.%u.%u", (unsigned)FW_VER_MAJOR, (unsigned)FW_VER_MINOR,
                  (unsigned)FW_VER_PATCH);
         lv_label_set_text(s_lbl_version, buf);
     }
@@ -63,22 +63,6 @@ lv_obj_t *screen_about_create(lv_display_t *disp)
     lv_obj_set_flex_align(column, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
     lv_obj_clear_flag(column, LV_OBJ_FLAG_SCROLLABLE);
 
-    lv_obj_t *card_fw = lv_obj_create(column);
-    lv_obj_remove_style_all(card_fw);
-    lv_obj_set_width(card_fw, lv_pct(100));
-    lv_obj_set_height(card_fw, LV_SIZE_CONTENT);
-    lv_obj_set_style_pad_all(card_fw, 28, LV_PART_MAIN);
-    lv_obj_set_style_bg_color(card_fw, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(card_fw, LV_OPA_80, LV_PART_MAIN);
-    lv_obj_set_style_radius(card_fw, 24, LV_PART_MAIN);
-    lv_obj_clear_flag(card_fw, LV_OBJ_FLAG_SCROLLABLE);
-
-    s_lbl_version = lv_label_create(card_fw);
-    lv_obj_set_style_text_font(s_lbl_version, &lv_font_montserrat_24, LV_PART_MAIN);
-    lv_obj_set_style_text_color(s_lbl_version, lv_color_hex(0x1C1C1E), LV_PART_MAIN);
-    lv_label_set_long_mode(s_lbl_version, LV_LABEL_LONG_MODE_WRAP);
-    lv_obj_set_width(s_lbl_version, lv_pct(100));
-
     lv_obj_t *card_mac = lv_obj_create(column);
     lv_obj_remove_style_all(card_mac);
     lv_obj_set_width(card_mac, lv_pct(100));
@@ -99,6 +83,26 @@ lv_obj_t *screen_about_create(lv_display_t *disp)
     lv_obj_set_style_text_align(s_lbl_mac, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
     lv_label_set_long_mode(s_lbl_mac, LV_LABEL_LONG_MODE_WRAP);
     lv_obj_set_width(s_lbl_mac, lv_pct(100));
+
+    lv_obj_t *card_fw = lv_obj_create(column);
+    lv_obj_remove_style_all(card_fw);
+    lv_obj_set_width(card_fw, lv_pct(100));
+    lv_obj_set_height(card_fw, LV_SIZE_CONTENT);
+    lv_obj_set_style_pad_all(card_fw, 28, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(card_fw, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(card_fw, LV_OPA_80, LV_PART_MAIN);
+    lv_obj_set_style_radius(card_fw, 24, LV_PART_MAIN);
+    lv_obj_set_layout(card_fw, LV_LAYOUT_FLEX);
+    lv_obj_set_flex_flow(card_fw, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_align(card_fw, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+    lv_obj_clear_flag(card_fw, LV_OBJ_FLAG_SCROLLABLE);
+
+    s_lbl_version = lv_label_create(card_fw);
+    lv_obj_set_style_text_font(s_lbl_version, &lv_font_montserrat_24, LV_PART_MAIN);
+    lv_obj_set_style_text_color(s_lbl_version, lv_color_hex(0x1C1C1E), LV_PART_MAIN);
+    lv_obj_set_style_text_align(s_lbl_version, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN);
+    lv_label_set_long_mode(s_lbl_version, LV_LABEL_LONG_MODE_WRAP);
+    lv_obj_set_width(s_lbl_version, lv_pct(100));
 
     about_refresh_labels();
     (void)ui_status_bar_create(scr);
